b29c3faeafb623b77637e9089ded33478509d80a
clement <clement.saunier@epitech.eu>
2022-05-18 18:54:27 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './component/page/Home';
+import Page from './component/page/Page';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Page>
+        <Home/>
+      </Page>
     </div>
   );
 }
---
+++ src/component/page/Home.jsx
@@ -0,0 +1,18 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Who am I ?</h1>
+            <p>
+                <a href="https://twitter.com/sheykeidev" target="_blank" rel="noreferrer">@sheykeidev</a>: student developer, web3 and gamedev enthusiast.
+            </p>
+            <h1>Where are we ?</h1>
+            <p>
+                On my personal developer website where can I experiment about web3 and share cool things.
+            </p>
+            <h1>Why is it so empty ?</h1>
+            <p>
+                This is just some boilerplate code that I uploaded to IPFS and linked my ENS to. It's my first step, you're early ;).
+            </p>
+        </>
+    )
+}
